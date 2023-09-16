41b927762300a87b01d8cda8dd94d3047b123847
AnshulSood11 <anshulsoodmail@gmail.com>
2023-09-16 15:20:35 +0530

    session and  cellClick handling

--- client/src/pages/game.tsx
+++ client/src/pages/game.tsx
@@ -1,16 +1,37 @@
 import {io} from "socket.io-client";
 
 export default function Game() {
-  function handleClick() {
-    console.log("Event emitted.")
-    var socket = io("http://localhost:5000", {});
-    let data = {name: "Buddy"};
-    socket.emit('cellClick', data);
-  }
 
-  return (
-    <div>
-      <button onClick={handleClick}>Hello Buddy!</button>
-    </div>
-  )
+    var socket = io("http://localhost:5000", {});
+    function handleCellClick() {
+        let data = {row: 0, col: 0};
+        socket.emit('cellClick', data);
+        console.log("Cell Click Event emitted.", data)
+    }
+
+    function handleCreateSession() {
+        socket.emit('createSession', {});
+        console.log("Create session event emitted.")
+    }
+
+    let sessionId = ''
+    function handleInput(event: any) {
+        sessionId = event.target.value;
+    }
+    function handleOnBlur(event: any) {
+    }
+    function handleJoinSession() {
+        console.log("Join session event emitted.", sessionId)
+        socket.emit('joinSession', {sessionId: sessionId});
+    }
+
+    return (
+        <div>
+            <button onClick={handleCellClick}>Cell Click</button>
+            <button onClick={handleCreateSession}>Create Session</button>
+            <button onClick={handleJoinSession}>Join Session</button>
+            <label htmlFor="name">Name (4 to 8 characters):</label>
+            <input type="text" id="name" name="name" onChange={(e) => handleInput(e)} onBlur={handleOnBlur}/>
+        </div>
+    )
 }
--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const socket = require("socket.io");
 const PORT = 5000;
 const app = express();
 const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
+    console.log(`Listening on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
 
 // Static files
@@ -15,17 +15,85 @@ app.use(express.static("public"));
 // Socket setup
 const io = socket(server, {
     cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    //   allowedHeaders: ["my-custom-header"],
-    //   credentials: true
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
     }
 });
 
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
 
-  socket.on("cellClick", function (data) {
-    console.log(data);
-  });
+const players = new Map()
+const sessions = new Map();
+io.on("connection", function (socket) {
+    console.log("Made socket connection", socket.id);
+    let playerId = socket.id;
+    let currPlayer;
+    if (!players.has(playerId)) {
+        let newPlayer = {
+            playerId: playerId,
+            symbol: null,
+            sessionId: null
+        };
+        players.set(playerId, newPlayer);
+        currPlayer = newPlayer;
+    } else {
+        currPlayer = players.get(playerId);
+    }
+
+    socket.on("createSession", function (data) {
+        console.log("createSession", data, currPlayer);
+        let sessionId = Math.random().toString(36).substring(2, 7);
+        let newSession = {
+            sessionId: sessionId,
+            player1: playerId,
+            player2: null,
+            player1Moves: [],
+            player2Moves: []
+        }
+        sessions.set(sessionId, newSession);
+
+        currPlayer.symbol = 'x';
+        currPlayer.sessionId = sessionId;
+    });
+
+    socket.on("joinSession", function (data) {
+        console.log("joinSession", data, currPlayer);
+        let sessionIdToJoin = data.sessionId;
+        if (sessions.has(sessionIdToJoin)) {
+            let session = sessions.get(sessionIdToJoin);
+            if (session.player1 === playerId) {
+                socket.emit('exception', {errorMessage: 'Session already joined'});
+                console.log('Session already joined')
+            } else if (session.player2 != null) {
+                socket.emit('exception', {errorMessage: 'Session is full'});
+                console.log('Session is full')
+            } else {
+                session.player2 = playerId;
+                currPlayer.sessionId = sessionIdToJoin;
+                currPlayer.symbol = 'o';
+                console.log('Successfully joined session', session)
+            }
+        } else {
+            socket.emit('exception', {errorMessage: 'Invalid sessionId. Create a new session'});
+            console.log('Invalid sessionId. Create a new session')
+        }
+    });
+
+    socket.on("cellClick", function (data) {
+        console.log("cellClick", data, currPlayer);
+        let sessionId = currPlayer.sessionId;
+        if (!sessions.has(sessionId)) {
+            socket.emit('exception', {errorMessage: 'No session found for sessionId' + sessionId});
+            console.log('No session found for sessionId');
+        } else {
+            let session = sessions.get(sessionId);
+            if (session.player1 === playerId) {
+                session.player1Moves.push(data);
+            } else if (session.player2 === playerId) {
+                session.player2Moves.push(data);
+            } else {
+                socket.emit('exception', {errorMessage: 'No player found corresponding to playerId: ' + playerId});
+            }
+            console.log(session);
+        }
+    });
 });
